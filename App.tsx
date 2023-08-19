94b7b53b8dff34e88f326550c3458aae1b15bfcf
codergalib2005 <coderboygalif@gmail.com>
2023-08-19 13:48:55 +0600

    setup the project and add react native vector icon

--- App.tsx
+++ App.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
-import {Text, View} from 'react-native';
-import colors from './src/theme/colors';
-import fonts from './src/theme/fonts';
-import AntDesign from 'react-native-vector-icons/AntDesign';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
 
-const App = () => {
+export default function App() {
   return (
-    <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-      <Text
-        style={{
-          color: colors.primary,
-          fontSize: fonts.size.default,
-        }}>
-        Hello world
-        <AntDesign name="dribbble" size={fonts.size['xxlg']} />
-      </Text>
+    <View style={styles.post}>
+      {/* Post Header */}
+      <View style={styles.header}>
+        <Image
+          source={{
+            uri: 'https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png',
+          }}
+          style={styles.userAvatar}
+        />
+        <Text style={styles.userName}>codergalib2005</Text>
+        <Entypo
+          name="dots-three-horizontal"
+          size={16}
+          style={styles.threeDots}
+        />
+      </View>
+      {/* Post Content */}
+      <Image
+        source={{
+          uri: 'https://images.pexels.com/photos/2745258/pexels-photo-2745258.jpeg',
+        }}
+        style={styles.image}
+      />
+      {/* Post Footer */}
     </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  post: {},
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+  header: {},
+  userAvatar: {},
+  userName: {},
+  threeDots: {},
+});
